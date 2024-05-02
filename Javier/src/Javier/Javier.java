1895bd84926823c4d388c8a8b2c1435ba2124c33
Cristina Villahermosa <cristinavillahermosa32@gmail.com>
2024-05-02 12:24:54 +0200

    Agrega clase y metodo para Javier

---
+++ Javier/src/Javier/Javier.java
@@ -0,0 +1,17 @@
+package Javier;
+
+public class Javier {
+
+	public void Nombre() {
+		System.out.println("Hola soy Javier ");
+	}
+
+	public static void main(String[] args) {
+	
+		 Javier javier = new Javier();
+		 javier.Nombre();
+
+	}
+
+}
+
