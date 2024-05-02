5d8d90de5f31cbf93b508d67e437372aff3b8c26
Cristina Villahermosa <cristinavillahermosa32@gmail.com>
2024-05-02 12:21:46 +0200

    Agrega clase y metodo para Cristina

---
+++ Cristina/src/Cristina/Cristina.java
@@ -0,0 +1,17 @@
+package Cristina;
+
+public class Cristina {
+
+	public void Nombre() {
+		System.out.println("Hola soy Cristina ");
+	}
+
+	public static void main(String[] args) {
+	
+		Cristina cristina = new Cristina();
+		cristina.Nombre();
+
+	}
+
+}
+
